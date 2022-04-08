a5b27581a32f651695803c539a71241fda03f6f0
marck0101 <marcos.henrique.a7x@gmail.com>
2022-04-08 15:13:54 -0300

    extraindo dados de json

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,11 +1,21 @@
-function producaoMensal(){
+function consumoMensal(contaMes, tarifa) {
+    var contaMes = document.getElementById("contaMes").value;
+    var tarifa = document.getElementById("tarifa").value;
+    var divisao = 0;
+    var quo = Math.floor(contaMes / divisao);
+    var rem = contaMes % divisao;
+
+
+    if ((contaMes != 0) && (tarifa != 0)) {
+        divisao = contaMes / tarifa;
+        alert("A media do consumo mensal e " + divisao);
+
+    }
+}
+function potenciaInstalada() {
 
 }
 
-function potenciaInstalada(){
+function areaPesp() {
 
-}
-
-function areaPesp(){
-    
 }
