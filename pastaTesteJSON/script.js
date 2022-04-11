e2284e515a8eb6f4de6594d3ae6add94617eb68b
marck0101 <marcos.henrique.a7x@gmail.com>
2022-04-11 17:16:00 -0300

    add testes

---
+++ pastaTesteJSON/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+function capturaDados() {
+    // let irradiacao = "";
+    var localidade = "Santo Cristo";
+    // let uf = "";
+    
+
+    if (irradiacao != "" && localidade != "" && uf != "")
+        console.log("irradiação: " + irradiacao + "cidade: " + localidade + "estado: " + uf)
+}
