121a0223ee19d096a53ebd22ebf9ead588f8f790
marck0101 <marcos.henrique.a7x@gmail.com>
2022-04-12 13:41:31 -0300

    só pra limpar a barra lateral kkkk

--- calculos.js
+++ calculos.js
@@ -155,16 +155,16 @@ function consumoMensal() {
 //A função de irradJson vai buscar a média de irradiação do município setado  
 function irradJson() {
     // aqui está chamando os arquivos de outro local do projeto
-
+    // <script type="text/javascript" src="./Testejson.js"></script>
     // aqui chama o arquivo json que agora é um texto de volta para json
     var json = JSON.parse(irradiacao);
     // o var do município vai armazenar o município do cep que o cliente digitar
-    var municipio = "";
+    var localidade = document.getElementById("localidade").value;
 
     json.map((item) => {
         // o map vai buscar o item que o teste está carregando, no caso o número e vai printar no
         console
-        if ((item.NAME === "Panambi") && (item.STATE === "RIO GRANDE DO SUL")) {
+        if ((item.NAME === "localidade") && (item.STATE === "UF")) {
             console.log("A irradiação anual de", item.NAME, "é: ", item.ANNUAL)
         }
     });
